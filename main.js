d278b2741ad57608dd31000b3daf8383484fad10
George <sgeorge7731@gmail.com>
2019-05-20 17:06:58 +0300

    dots added

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+var container = document.getElementById('dots');
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var scrollArea = 5000 + windowHeight;
+var square1 = document.getElementsByClassName('main_wrap-dots')[0];
+var square2 = document.getElementsByClassName('main_wrap-dots')[1];
+
+
+// update position of square 1 and square 2 when scroll event fires.
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset || window.scrollTop;
+  var scrollPercent = scrollTop/scrollArea || 0;
+  
+  square1.style.top = scrollPercent*window.innerHeight + 'px';
+  square2.style.top = 800 - scrollPercent*window.innerHeight*2 + 'px';
+});
