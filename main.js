13a1295f563b49b15214f504aa9317a077880f14
George <sgeorge7731@gmail.com>
2019-05-21 15:32:45 +0300

    added services

--- main.js
+++ main.js
@@ -9,24 +9,28 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
-}
+};
 
 
-var container = document.getElementById('dots');
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-var scrollArea = 5000 + windowHeight;
-var square1 = document.getElementsByClassName('main_wrap-dots')[0];
-var square2 = document.getElementsByClassName('main_wrap-dots')[1];
 
 
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function() {
-  var scrollTop = window.pageYOffset || window.scrollTop;
-  var scrollPercent = scrollTop/scrollArea || 0;
+// var container = document.getElementById('header');
+// var windowHeight = window.innerHeight;
+// var windowWidth = window.innerWidth;
+// var scrollArea = 50 + windowHeight;
+// var square1 = document.getElementsByClassName('header-text')[0];
+// var square2 = document.getElementsByClassName('header-text')[1];
+
+
+// // update position of square 1 and square 2 when scroll event fires.
+// window.addEventListener('scroll', function() {
+//   var scrollTop = window.pageYOffset || window.scrollTop;
+//   var scrollPercent = scrollTop*scrollArea || 0;
   
-  square1.style.top = scrollPercent*window.innerHeight + 'px';
-  square2.style.top = 800 - scrollPercent*window.innerHeight*2 + 'px';
-});
+//   square1.style.left = scrollPercent/window.innerHeight + 'px';
+//   square2.style.left = 800 - scrollPercent*window.innerHeight*2 + 'px';
+// });
+
+
 
 new WOW().init();
